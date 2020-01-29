58213e935dc5df228f32a4d7ece1dde9e69501c5
Kostiantyn <podlesny@outlook.com>
2020-01-29 13:18:50 +0200

    build

---
+++ build/precache-manifest.59448dadfc10496bf0a09f7cce549e59.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294f3550f6d010200654a66c845988e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5118648cc3559b6be80b",
+    "url": "/static/css/main.a298de68.chunk.css"
+  },
+  {
+    "revision": "615f1911704847363d4a",
+    "url": "/static/js/2.37736586.chunk.js"
+  },
+  {
+    "revision": "fec8010374a5996c79e5975a9066a6ad",
+    "url": "/static/js/2.37736586.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5118648cc3559b6be80b",
+    "url": "/static/js/main.510baae3.chunk.js"
+  },
+  {
+    "revision": "dddf296d5349763f4064",
+    "url": "/static/js/runtime-main.15126ca4.js"
+  },
+  {
+    "revision": "5d45aed9c2daa0b1e4f17bad6eb96d13",
+    "url": "/static/media/facebook-icon.5d45aed9.svg"
+  },
+  {
+    "revision": "89ed37b3a469415a2338bb95ded72588",
+    "url": "/static/media/logo.89ed37b3.svg"
+  },
+  {
+    "revision": "96e9ea6aaaba9ff0260a66137b511dcf",
+    "url": "/static/media/playbuzz-icon.96e9ea6a.svg"
+  },
+  {
+    "revision": "addbf901a80970cff690fbd6a61a596f",
+    "url": "/static/media/url-icon.addbf901.svg"
+  },
+  {
+    "revision": "2e1498077e1d99768356c4b44199cd56",
+    "url": "/static/media/youtube-icon.2e149807.svg"
+  }
+]);
